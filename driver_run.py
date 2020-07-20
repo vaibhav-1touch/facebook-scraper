d99389f859766f96bd4b90e06b31062eeb22d2f2
vaibhav-1touch <67089226+vaibhav-1touch@users.noreply.github.com>
2020-07-20 17:57:27 +0530

    Update driver_run.py

--- driver_run.py
+++ driver_run.py
@@ -9,13 +9,16 @@ import time
 import pyautogui
 import random
 import os
+import urllib.request
+from tqdm import tqdm
+
+search = "unity3d"
 
 
-search = "DonaldTrump"
+post_counter = 0
 
-
-if not os.path.exists(search):
-    os.mkdir(search)
+if not os.path.exists(os.path.join(search, 'images')):
+    os.makedirs(os.path.join(search, 'images'))
 
 # Global variables
 post_class_css_selector = "._5pcr.userContentWrapper"
@@ -25,9 +28,11 @@ get_timestamp_span_dict = {"class": "_5ptz"}
 get_reactions_span_dict = {"class": "_1n9k"}
 get_comments_span_dict = {"class": "_3l3x"}
 get_num_share_anchor_dict = {"class": "_3rwx _42ft"}
+get_images_div_css_selector = ".uiScaledImageContainer"
 
 posts_dataframe_dict = OrderedDict()
 posts_dataframe_list = []
+posts_dataframe_capture = []
 
 def no_login():
     for i in range(1,3):
@@ -70,7 +75,7 @@ def get_reactions(i):
         for k in j.find_all('a'):
             if k['aria-label'].lower().find('care') != -1:
                 posts_dataframe_dict['care'] = k['aria-label']
-            if k['aria-label'].lower.find('love') != -1:
+            if k['aria-label'].lower().find('love') != -1:
                 posts_dataframe_dict['love'] = k['aria-label']
             if k['aria-label'].lower().find('likes') != -1:
                 posts_dataframe_dict['likes'] = k['aria-label']
@@ -80,6 +85,7 @@ def get_reactions(i):
                 posts_dataframe_dict['haha'] = k['aria-label']
             if k['aria-label'].lower().find('sad') != -1:
                 posts_dataframe_dict['sad'] = k['aria-label']
+
 def get_num_shares(i):
     for j in i.find_all('a', get_num_share_anchor_dict):
         posts_dataframe_dict['shares'] = j.text
@@ -109,18 +115,35 @@ def get_comments(i):
     except:
         pass
 
+def get_images(i):
+    global post_counter
+    image_count = 0
+    post_counter += 1
+    image_name_append = []
+
+    for j in i.find_elements_by_css_selector(get_images_div_css_selector):
+        for k in BeautifulSoup(j.get_attribute('innerHTML'), 'lxml'):
+            for l in k.find_all('img'):
+                image_count += 1
+                name_image = f'{search}/images/{post_counter}_{image_count}.jpg'
+                post_image_name = f'{post_counter}_{image_count}.jpg'
+                image_name_append.append(post_image_name)
+                urllib.request.urlretrieve(l['src'], name_image)
+    posts_dataframe_capture.append(image_name_append)
+
 def append_to_list():
     posts_dataframe_list.append(posts_dataframe_dict.copy())
     posts_dataframe_dict.clear()
 
 def save_to_file():
     filename = f'{search}.csv'
-    sector16 = pd.DataFrame(posts_dataframe_list, columns=['title', 'timestamp', 'likes', 'love', 'care', 'wow', 'haha', 'sad', 'shares', 'comments'])
+    sector16 = pd.DataFrame(posts_dataframe_list, columns=['title', 'timestamp', 'likes', 'love', 'care', 'wow', 'haha', 'sad', 'shares', 'images', 'comments'])
     if os.path.exists(filename):
         sector16.to_csv(os.path.join(search,filename), header=False, mode='a', index=False)
     else:
         sector16.to_csv(os.path.join(search,filename), header=False, mode='a', index=True)
 
+    posts_dataframe_capture.clear()
     posts_dataframe_list.clear()
 
 def scroll_to_bottom():
@@ -145,27 +168,29 @@ no_login()
 last_count_selenium = 0
 
 while(True):
-
     source_selenium = selenium_source_posts()
     length_selenium = len(source_selenium)
     
-    for source in source_selenium[last_count_selenium:length_selenium]:
+    print(f'Posts to be scraped : {len(source_selenium)}')
+    for source in tqdm(source_selenium[last_count_selenium:length_selenium]):
         extend_comments(source)
-    
+        get_images(source)
+
     source_beautiful = beautiful_source_posts(source_selenium[last_count_selenium:length_selenium])
-    print(len(source_beautiful))
     if(len(source_beautiful) > 0):
-        for source in source_beautiful:
+        for count, source in enumerate(source_beautiful):
             get_title(source)
             get_timestamp(source)
             get_reactions(source)
             get_num_shares(source)
             get_comments(source)
 
+            posts_dataframe_dict['images'] = posts_dataframe_capture[count]
+
             append_to_list()
 
         save_to_file()
-        print(f"Number of posts : {length_selenium}")
+        print(f"Number of posts : {length_selenium}         Tally images post : {post_counter}")
         print('Saving to file')
 
         last_count_selenium = length_selenium
